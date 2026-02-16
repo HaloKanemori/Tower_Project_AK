c9bc8bb8492ea2ed0daeeff2d2788344c2ad989b
HaloKanemori <rl99712@mail.harpercollege.edu>
2026-02-15 23:58:19 -0600

    Finished

--- Tower_Project_AK/Tower_Project_AK.cpp
+++ Tower_Project_AK/Tower_Project_AK.cpp
@@ -13,6 +13,7 @@ const int ENEMY = 5;
 const int HEALTH = 6;
 const int STR = 7;
 const int DEF = 8;
+const int PORT = 9;
 
 class Player {
 public:
@@ -43,9 +44,25 @@ private:
     int cols;
     int** grid;  //2D like vector vector
     string name;
+    vector<int> teleR1, teleC1;
+    vector<int> teleR2, teleC2;
 
 public:
     Player player;
+
+    void addTelePair(int r1, int c1, int r2, int c2) {
+        if (r1 >= 0 && r1 < rows && c1 >= 0 && c1 < cols &&
+            r2 >= 0 && r2 < rows && c2 >= 0 && c2 < cols) {
+
+            grid[r1][c1] = PORT;
+            grid[r2][c2] = PORT;
+
+            teleR1.push_back(r1);
+            teleC1.push_back(c1);
+            teleR2.push_back(r2);
+            teleC2.push_back(c2);
+        }
+    }
 
     Dungeon(int r, int c, string n) {
         rows = r;
@@ -117,6 +134,9 @@ public:
                     else if (tile == DEF) {
                         cout << "F ";
                     }
+                    else if (tile == PORT) {
+                        cout << "T ";
+                    }
                     else {
                         cout << "? ";  // error val
                     }
@@ -129,8 +149,12 @@ public:
         cout << "\nHP: " << player.HP
             << " STR: " << player.str
             << " DEF: " << player.def
-            << " Keys: " << player.key
             << endl;
+        cout << "Keys:[ ";
+        for (int k = 0; k < player.key; k++) {
+            cout << "# ";
+        }
+        cout << ']' << endl;
     }
     void display_edit() {//only differance is the numbers for ease of placing in the editor.
         cout << endl << "--- " << name << " ---" << endl;
@@ -174,6 +198,9 @@ public:
 
                     else if (tile == DEF) {
                         cout << "F ";
+                    }
+                    else if (tile == PORT) {
+                        cout << "T ";
                     }
                     else {
                         cout << "? ";  // error val
@@ -282,6 +309,22 @@ public:
             cout << "You escape\n";
             return false;
         }
+        if (tile == PORT) {
+            for (int i = 0; i < teleR1.size(); i++) {
+                if (newRow == teleR1[i] && newCol == teleC1[i]) { //first
+                    cout << "You step on a teleporter!\n";
+                    player.row = teleR2[i];
+                    player.col = teleC2[i];
+                    return true;
+                }
+                if (newRow == teleR2[i] && newCol == teleC2[i]) {//other
+                    cout << "You step on a teleporter!\n";
+                    player.row = teleR1[i];
+                    player.col = teleC1[i];
+                    return true;
+                }
+            }
+        }
         // end of tile interactions
         player.row = newRow;
         player.col = newCol;
@@ -334,7 +377,7 @@ void playDungeon(Dungeon* d) {
 }
 
 
-// makes the first 2 levels
+// makes the first 3 levels
 Dungeon* createBasic() {
     Dungeon* d = new Dungeon(5, 5, "Tutorial");
     d->placePlayer(0, 0);
@@ -357,6 +400,17 @@ Dungeon* createLV2() {
     d->setTile(0, 4, DOOR);
     return d;
 }
+Dungeon* createPort() {
+    Dungeon* d = new Dungeon(6, 6, "Port test");
+    d->placePlayer(0, 0);
+    d->addTelePair(1, 1, 2, 2);
+    d->setTile(3, 1, HEALTH);
+    d->setTile(5, 5, GOAL);
+    d->setTile(0, 3, KEY);
+    d->setTile(0, 4, DOOR);
+    return d;
+}
+
 
 Dungeon* createCustomDungeon() {
     int r, c;
@@ -443,7 +497,7 @@ Dungeon* createCustomDungeon() {
         cin >> choice;
 
         if (choice == 1) {
-            cout << "\n1) Wall\n2) Goal\n3) Key\n4) Door\n5) Enemy\n6) Health\n7) Strength\n8) Defense\n";
+            cout << "\n1) Wall\n2) Goal\n3) Key\n4) Door\n5) Enemy\n6) Health\n7) Strength\n8) Defense\n9) Teleporter";
             cout << "Select object: ";
 
             int obj;
@@ -451,7 +505,7 @@ Dungeon* createCustomDungeon() {
             do {
                 cin >> obj;
 
-                if (cin.fail()||obj<1||obj>8) {
+                if (cin.fail()||obj<1||obj>9) {
                     cin.clear();
                     cin.ignore(numeric_limits<streamsize>::max(), '\n');
                     loop = false;
@@ -461,40 +515,44 @@ Dungeon* createCustomDungeon() {
                     loop = true;
                 }
             } while (!loop);
-            int row, col;
-            cout << "Row: ";
-            do {
-                cin >> row;
+            if (obj == PORT) {
 
-                if (cin.fail()) {
-                    cin.clear();
-                    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-                    loop = false;
-                    cout << "\nInvalid row. Please enter an integer" << endl;
+                int r1, c1, r2, c2;
+
+                cout << "First teleporter row: ";
+                cin >> r1;
+                cout << "First teleporter col: ";
+                cin >> c1;
+
+                cout << "Second teleporter row: ";
+                cin >> r2;
+                cout << "Second teleporter col: ";
+                cin >> c2;
+
+                if (r1 >= 0 && r1 < r && c1 >= 0 && c1 < c &&
+                    r2 >= 0 && r2 < r && c2 >= 0 && c2 < c) {
+
+                    d->addTelePair(r1, c1, r2, c2);
                 }
                 else {
-                    loop = true;
+                    cout << "Invalid teleporter positions!" << endl;
                 }
-            } while (!loop);
-            cout << "Col: ";
-            do {
+            }
+            else {
+
+                int row, col;
+
+                cout << "Row: ";
+                cin >> row;
+                cout << "Col: ";
                 cin >> col;
 
-                if (cin.fail()) {
-                    cin.clear();
-                    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-                    loop = false;
-                    cout << "\nInvalid column. Please enter an integer" << endl;
-                }
-                else {
-                    loop = true;
-                }
-            } while (!loop);
+                if (row >= 0 && row < r && col >= 0 && col < c)
+                    d->setTile(row, col, obj);
+                else
+                    cout << "Invalid position" << endl;
+            }
 
-            if (row >= 0 && row < r && col >= 0 && col < c)
-                d->setTile(row, col, obj);
-            else
-                cout << "Invalid position" << endl;;
         }
         else if (choice == 2) {
             cout << "Enter dungeon name: ";
@@ -514,6 +572,7 @@ int main()
 
     dungeons.push_back(createBasic());
     dungeons.push_back(createLV2());
+    dungeons.push_back(createPort());
 
     bool run = true;
     while (run) {
